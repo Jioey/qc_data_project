9b3d89588d48e6660403768b56f83e30e155cfe2
Joey <55892851+JioeyKun@users.noreply.github.com>
2022-09-06 11:06:12 -0700

    feat: w classes, makes a more functional main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """##Packages"""
+import gui
 import dataParser
 
+'''
+For 901 update of the TC-201
+'''
 if __name__ == '__main__':
-    main()
+    # root of tkinter
+    ui = gui.App(dataParser.dataParser())
+    # mainloop continuosly shows window until closed
+    ui.mainloop()
