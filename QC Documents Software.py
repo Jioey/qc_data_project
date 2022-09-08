fb1219013de518915519f27d8f5e45bf158a681c
Joey <55892851+JioeyKun@users.noreply.github.com>
2022-09-08 16:33:00 -0700

    feat: changes const to class structure

--- QC Documents Software.py
+++ QC Documents Software.py
@@ -5,11 +5,13 @@ Refrence: https://www.datacamp.com/tutorial/docstrings-python
 '''
 import gui
 import dataParser
+import constants
 
 '''
 For 901 update of the TC-201
 '''
 if __name__ == '__main__':
+    c = constants()
     # root of tkinter
     ui = gui.App(dataParser.dataParser())
     # mainloop continuosly shows window until closed
