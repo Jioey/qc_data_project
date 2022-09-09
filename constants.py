0ad502781be1446dfd9a5cf850c8a441f43c8a5a
Joey <55892851+JioeyKun@users.noreply.github.com>
2022-09-09 14:24:50 -0700

    misc: deprecates reading const from txt

--- constants.py
+++ constants.py
@@ -11,19 +11,7 @@ class constants():
       with open("constants.txt", "r") as f:
          fList = f.readlines()
 
-      self.PARAMETERS = strToList(fList[1])
-      # print(self.PARAMETERS)
-
-      self.COA_TEMPLATE_NAME, self.LABSHEET_TEMPLATE_NAME, self.COA_FAILED_TEMPLATE_NAME, self.LABSHEET_FAILED_TEMPLATE_NAME = strToList(fList[4])
-      # # print(self.LABSHEET_FAILED_TEMPLATE_NAME)
-
-      self.allowedRangesI = strToList(fList[8], True)
-      self.allowedRangesII = strToList(fList[10], True)
-      self.allowedRangesIII = strToList(fList[12], True)
-      # print(self.allowedRangesI)
-
-      return True
-
+      self.loadYaml()
 
 '''
 Turns a list in string format into list datatype
@@ -35,32 +23,13 @@ Args:
 Returns:
    list: str in list format
 '''
-def strToList(s:str, is2D:bool=False) -> list:
-   # strips whitespace from both ends
-   s = s.strip()
-
-   # split based on comma
-   if not is2D:
-      # strips [] from both ends, if exsists
-      s = s.replace('[', '')
-      s = s.replace(']', '')
-      l = s.split(', ')
-      # strip '' from both ends of each entry
-      for i in range(len(l)):
-         l[i] = l[i][1:len(l[i])-1]
-   else:
-      # strips [] from both ends, if exsists
-      s = s.replace('[[', '')
-      s = s.replace(']]', '')
-      l = s.split('], [')
-      # turn inside str to list
-      for i in range(len(l)):
-         l[i] = strToList(l[i])
+      kovaIII = tolerenceRanges[3][1:11]
 
    # print(l)
    return l
 
 
+# for debug use
 if __name__ == '__main__':
    c = constants()
-   c.readConstants()
+   c.loadYaml()
